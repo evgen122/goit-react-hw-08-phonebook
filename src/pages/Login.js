23d96d4d247f3a1a66494c95fd7b2d638afbdea5
evgen122 <evg.khristenko@gmail.com>
2023-11-22 08:32:17 +0200

    register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,1 +1,12 @@
-export default function Login() {}
+import { LoginForm } from 'components/LoginForm/LoginForm';
+
+export default function Login() {
+  return (
+    <div>
+      {/* <Helmet> */}
+      <title>Login</title>
+      {/* </Helmet> */}
+      <LoginForm />
+    </div>
+  );
+}
